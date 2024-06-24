5d06a3ab1436cb6d3f360dcba8349cb926ff9d3b
Tong-Kiat Tan <2617095+tongkiat@users.noreply.github.com>
2024-06-24 12:33:19 +0800

    Update koans

--- koans/path_to_enlightenment.rb
+++ koans/path_to_enlightenment.rb
@@ -3,21 +3,21 @@
 $LOAD_PATH << File.dirname(__FILE__)
 
 require 'about_asserts'
-require 'about_nil'
-require 'about_objects'
-require 'about_arrays'
-require 'about_array_assignment'
-require 'about_hashes'
+require 'about_true_and_false'
 require 'about_strings'
 require 'about_symbols'
-require 'about_regular_expressions'
+require 'about_arrays'
+require 'about_array_assignment'
+require 'about_objects'
+require 'about_nil'
+require 'about_hashes'
 require 'about_methods'
-in_ruby_version("2") do
+in_ruby_version("2", "3") do
   require 'about_keyword_arguments'
 end
 require 'about_constants'
+require 'about_regular_expressions'
 require 'about_control_statements'
-require 'about_true_and_false'
 require 'about_triangle_project'
 require 'about_exceptions'
 require 'about_triangle_project_2'
@@ -37,5 +37,8 @@ require 'about_proxy_object_project'
 require 'about_to_str'
 in_ruby_version("jruby") do
   require 'about_java_interop'
+end
+in_ruby_version("2.7") do
+  require 'about_pattern_matching'
 end
 require 'about_extra_credit'
